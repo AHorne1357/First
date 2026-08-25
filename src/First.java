55f0d4ab73868fbe7da670b7e1eed1bd6dde6022
AHorne1357 <horneaustin810@gmail.com>
2026-08-24 23:48:04 -0400

    Initial commit

---
+++ src/First.java
@@ -0,0 +1,5 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.print("Hello World \n Job Title: Information Security Analyst \n Why: Job Opportunities \n Median Salary: $124,910");
+    }
+}
